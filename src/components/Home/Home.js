df59555e66d782fa5f20a3d0d7e7e2b553bbb668
pramod ade <prmdextracthere@gmail.com>
2024-02-03 17:26:06 +0530

    context API

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,10 +3,12 @@ import React from 'react';
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
 
+
 const Home = (props) => {
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+     
     </Card>
   );
 };
